1023ee9dc9a24ba538c5d95716c9b1ad19ac1474
akthecoders <akshay@akthecoder.com>
2021-03-10 17:47:33 +0530

    MultiModule Project Setup complete Closes #1

--- pet-clinic-web/src/main/java/akshay/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/akshay/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,16 +1,13 @@
 package akshay.springframework.sfgpetclinic;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx;
-        ctx = run(SfgPetClinicApplication.class, args);
+        SpringApplication.run(SfgPetClinicApplication.class, args);
     }
 
 }
